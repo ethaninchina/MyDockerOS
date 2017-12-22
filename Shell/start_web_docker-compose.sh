60fe94b09afc8dee6677c24e921ff041887c3729
station19 <wuyuzai@qq.com>
2017-12-22 14:45:37 +0800

    Update start_web_docker-compose.sh

--- Shell/start_web_docker-compose.sh
+++ Shell/start_web_docker-compose.sh
@@ -49,7 +49,7 @@ systemctl enable docker.service
 systemctl start docker.service
 
 #安装docker-compose编排服务
-curl -L https://github.com/docker/compose/releases/download/1.18.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
 if [ $? -ne  0 ]
     then
     echo "docker-compose 下载安装失败"
