1082369544ef38585d7e5cbd6372775049b0699b
station19 <wuyuzai@qq.com>
2018-03-29 11:41:08 +0800

    Create start_jenkins_docker.sh

---
+++ Docker/docker-compose/jenkins/start_jenkins_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+#CentOS 7.4 (tx cloud)
+
+#安装docker 和docker-compose
+yum install docker docker-compose -y
+
+/bin/systemctl start docker.service
+/bin/systemctl enable docker.service
+
+docker pull daocloud.io/library/jenkins:latest
+wget https://raw.githubusercontent.com/station19/MyDockerOS/master/Docker/docker-compose/jenkins/docker-compose.yml
+
+#启动
+docker-compose up -d
+
+#下载最新版jenkins
+wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+docker cp jenkins.war jenkins:/usr/share/jenkins/
+docker restart jenkins
+
+#docker commit $(docker ps|grep -v "CONTAINER ID"|awk '{print $1}') daocloud.io/library/jenkins
